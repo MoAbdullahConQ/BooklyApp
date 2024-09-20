021e697bc9138a2ed578f69cb2e220ad7d6610f8
Mhmed <noorelsayd193@gmail.com>
2024-09-20 15:47:11 +0300

    add custom list view item to home view body

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,17 +1,19 @@
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
+      // crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        CustomAppBar()
+        const CustomAppBar(),
+        CustomListViewItem(),
       ],
     );
   }
 }
-
