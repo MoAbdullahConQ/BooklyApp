595a643ad6eaaee3df31a92bb0018bf9fcd301fd
Mhmed <noorelsayd193@gmail.com>
2024-11-09 06:15:41 +0200

    create SimilarBooksListView

--- lib/Features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_item.dart
@@ -9,7 +9,7 @@ class CustomBookImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 2.7 / 4,
+      aspectRatio: 2.6 / 4,
       // aspectRatio: MediaQuery.of(context).size.width / 580,
       child: Container(
         // width: MediaQuery.of(context).size.width / 10,
