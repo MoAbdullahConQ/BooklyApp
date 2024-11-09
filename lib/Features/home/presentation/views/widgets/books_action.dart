86a45a143ac0c687f1e85baa613db23082582647
Mhmed <noorelsayd193@gmail.com>
2024-11-09 06:01:09 +0200

    create books button action

---
+++ lib/Features/home/presentation/views/widgets/books_action.dart
@@ -0,0 +1,34 @@
+import 'package:bookly_app/core/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+
+class BooksAction extends StatelessWidget {
+  const BooksAction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        children: [
+          Expanded(
+            child: CustomButton(
+              text: '19.99€',
+              txtColor: Colors.black,
+              bkColor: Colors.white,
+              borderRadius: BorderRadius.horizontal(left: Radius.circular(16)),
+            ),
+          ),
+          Expanded(
+            child: CustomButton(
+              text: 'Free Preview',
+              fontSize: 16,
+              txtColor: Colors.white,
+              bkColor: Color(0xffEF8262),
+              borderRadius: BorderRadius.horizontal(right: Radius.circular(16)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,37 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.text,
+    this.txtColor,
+    this.bkColor,
+    this.borderRadius, this.fontSize,
+  });
+
+  final String text;
+  final Color? txtColor, bkColor;
+  final BorderRadius? borderRadius;
+ final double? fontSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 48,
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+            backgroundColor: bkColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: borderRadius ?? BorderRadius.circular(12),
+            )),
+        child: Text(
+          text,
+          style: Styles.textStyle18.copyWith(
+              color: txtColor, fontWeight: FontWeight.w900, fontSize: fontSize),
+        ),
+      ),
+    );
+  }
+}
