d2de3e39d60086619c991dd414e18665cba2b676
Mhmed <noorelsayd193@gmail.com>
2024-11-13 22:39:27 +0200

    Implement fetch newest books method

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:bookly_app/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failures, BookModel>> fetchBestSellerBooks();
+  Future<Either<Failures, BookModel>> fetchNewestBooks();
   Future<Either<Failures, BookModel>> fetchFeaturedBooks();
 }
