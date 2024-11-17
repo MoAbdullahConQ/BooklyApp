609e700ec8706e72ea33fb5c4178eac34ecc3394
Mhmed <noorelsayd193@gmail.com>
2024-11-17 19:18:23 +0200

    cached network image

--- lib/Features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -7,21 +8,30 @@ class CustomBookImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      // aspectRatio: MediaQuery.of(context).size.width / 580,
-      child: Container(
-        // width: MediaQuery.of(context).size.width / 10,
-        // height: MediaQuery.of(context).size.height / 10,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          // color: const Color(0xffFEBDA6),
-          image: DecorationImage(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+          aspectRatio: 2.6 / 4,
+          // aspectRatio: MediaQuery.of(context).size.width / 580,
+          child: CachedNetworkImage(
             fit: BoxFit.fill,
-            image: NetworkImage(imgUrl),
-          ),
-        ),
-      ),
+            imageUrl: imgUrl,
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          )),
     );
   }
 }
+
+
+// Container(
+//         // width: MediaQuery.of(context).size.width / 10,
+//         // height: MediaQuery.of(context).size.height / 10,
+//         decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(16),
+//           // color: const Color(0xffFEBDA6),
+//           image: DecorationImage(
+//             fit: BoxFit.fill,
+//             image: NetworkImage(imgUrl),
+//           ),
+//         ),
+//       ),
