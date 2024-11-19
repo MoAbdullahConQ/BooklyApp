01f217e0f136a9cc82efdc84fc85637a753c0dd4
Mhmed <noorelsayd193@gmail.com>
2024-11-19 09:06:39 +0200

    intergrate Search cubit with ui BlocBuilder

--- lib/Features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/Features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,20 +1,36 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/books_list_view_item.dart';
+import 'package:bookly_app/Features/search/presentation/manager/search_cubit/search_cubit.dart';
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indecator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchResultListView extends StatelessWidget {
   const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      // physics: const NeverScrollableScrollPhysics(),
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          // child: BookListViewItem(bookModel: ,),
-          child: Text('data'),
-        );
+    return BlocBuilder<SearchCubit, SearchState>(
+      builder: (context, state) {
+        if (state is SearchSuccess) {
+          return ListView.builder(
+            // physics: const NeverScrollableScrollPhysics(),
+            padding: EdgeInsets.zero,
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                // child: BookListViewItem(bookModel: ,),
+                child: Text('data'),
+              );
+            },
+          );
+        } else if (state is SearchFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndecator();
+        }
       },
     );
   }
